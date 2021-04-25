e65bb954ae654bdabd937e303a277dfc12bb94a5
Maria Serobabina <mariaserobabina@gmail.com>
2021-04-25 02:59:32 -0400

    Adding changes to the code

--- src/Another_Day_1/LearningExperience.java
+++ src/Another_Day_1/LearningExperience.java
@@ -4,5 +4,6 @@ public class LearningExperience {
     public static void main(String[] args) {
         System.out.println("The spring has finally come.");
         System.out.println("Hello World");
+        return;
     }
 }
